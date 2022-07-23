ec7c109c8d170d4064370cee2fdeb383218c1854
nikitha-durgapu <61273976+nikitha-durgapu@users.noreply.github.com>
2022-07-23 20:51:18 +0530

    Update scriptnew.js

--- scriptnew.js
+++ scriptnew.js
@@ -4,18 +4,4 @@ $('.menu-btn').click(function() {
   $('.navb .menu').toggleClass("active");
   $('.menu-btn i').toggleClass("active");
 });
-const skillsContent = document.getElementsByClassName('skill_content'),
-      skillsHeader = document.querySelectorAll('.skill_header')
-function toggleskills() {
-  let itemClass = this.parentNode.className
-  for(i=0; i<skillsContent.length; i++){
-    skillsContent[i].className = 'skill_content skills_close'
-  }
-  if(itemClass === 'skill_content skills_close'){
-    this.parentNode.className = 'skill_content skills_open'
-  }
-}
-skillsHeader.forEach((el) => {
-  el.addEventListener('click', toggleskills)
-});
 });
